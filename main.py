16a49fb8974da9076f467b82a251be5d3b0be0de
MeltraNS <sgreaves7500@gmail.com>
2024-09-29 00:51:52 -0400

    solved word count

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():#here's what to do when the function first starts
     book_path = "books/frankenstein.txt"#set book_path as the string of the book we want to check out
     text = get_book_text(book_path) #what we want to print out is the strings
-    print(text)
+    #print(text)
     print(word_count(text))
 
 
@@ -10,6 +10,6 @@ def get_book_text(path): #define get book text with string path as an argument
         return f.read()
 
 def word_count(text):
-    return len(" ".split(text))
+    return len(text.split())
 
 main()
