665435c264c529636fdda0258f6187468b24f208
MeltraNS <sgreaves7500@gmail.com>
2024-09-29 00:47:31 -0400

    new idea to count words

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-    print(file_contents)
+def main():#here's what to do when the function first starts
+    book_path = "books/frankenstein.txt"#set book_path as the string of the book we want to check out
+    text = get_book_text(book_path) #what we want to print out is the strings
+    print(text)
+    print(word_count(text))
 
-def word_count(file_contents):
-    print(len(" ".split(file_contents)))
+
+def get_book_text(path): #define get book text with string path as an argument
+    with open(path) as f:
+        return f.read()
+
+def word_count(text):
+    return len(" ".split(text))
+
+main()
